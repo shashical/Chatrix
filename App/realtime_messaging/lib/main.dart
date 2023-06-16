8235f9ddc973b2abe646fccdfc38241096ffa53a
shashical <shashwatanandam@gmail.com>
2023-06-16 21:29:28 +0530

    updated main

--- App/realtime_messaging/lib/main.dart
+++ App/realtime_messaging/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-//hey
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter',
+      title: 'Flutter app',
       theme: ThemeData(
         // This is the theme of your application.
         //
