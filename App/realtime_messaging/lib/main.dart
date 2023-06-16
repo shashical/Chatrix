3ba1096f912f540c04a84ae86dc376f42089cd30
shashical <shashwatanandam@gmail.com>
2023-06-16 23:39:50 +0530

    upd

--- App/realtime_messaging/lib/main.dart
+++ App/realtime_messaging/lib/main.dart
@@ -41,11 +41,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 11;
-<<<<<<< Updated upstream
-  int _counter = 1;
-=======
   int _counter = 3;
->>>>>>> Stashed changes
 
   void _incrementCounter() {
     setState(() {
