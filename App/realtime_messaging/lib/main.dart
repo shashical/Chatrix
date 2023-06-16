f8005a1fba1a272e76fb3a7d6415bccfaa8184bc
rai-rajeev <106883666+rai-rajeev@users.noreply.github.com>
2023-06-16 23:47:45 +0530

    Update main.dart

--- App/realtime_messaging/lib/main.dart
+++ App/realtime_messaging/lib/main.dart
@@ -42,14 +42,11 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 11;
   int _counter = 5;
+  int _counter = 3;
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
+
       _counter+=3;
     });
   }
