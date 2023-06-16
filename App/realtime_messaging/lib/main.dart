710edf907ee612608140f1069a30518d8be2c428
shashical <shashwatanandam@gmail.com>
2023-06-16 22:45:51 +0530

    upd

--- App/realtime_messaging/lib/main.dart
+++ App/realtime_messaging/lib/main.dart
@@ -97,7 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times: Rajeev Rai',
+              'You have pushed the button this many times:',
             ),
             Text(
               '$_counter',
