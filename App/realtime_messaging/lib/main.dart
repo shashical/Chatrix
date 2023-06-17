e359d79eb0cc8f6531a1e10f44df455aaa407212
rai-rajeev <106883666+rai-rajeev@users.noreply.github.com>
2023-06-17 21:46:14 +0530

    changes-commit

--- App/realtime_messaging/lib/main.dart
+++ App/realtime_messaging/lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 //hey
-void main() {
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -41,7 +47,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 11;
-  int _counter = 5;
+
 
   void _incrementCounter() {
     setState(() {
