ed71dce05464de0ae30c45919e9e241862663488
rai-rajeev <106883666+rai-rajeev@users.noreply.github.com>
2023-06-17 22:57:24 +0530

    firebase-integration-done

--- App/realtime_messaging/lib/main.dart
+++ App/realtime_messaging/lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 //hey
 void main()async{
+  final ref=FirebaseFirestore.instance;
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
