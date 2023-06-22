214c573e06abb00f01d389b86a7923cc5941d0ae
rai-rajeev <106883666+rai-rajeev@users.noreply.github.com>
2023-06-22 22:56:35 +0530

    updated search-contact

--- App/realtime_messaging/lib/main.dart
+++ App/realtime_messaging/lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home:SplashPage(),
+      home:SearchContactPage()//SplashPage(),
     );
   }
 }
